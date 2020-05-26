0fa6627e9ea0357ec3b27958ab8b4761f14e150f
santiago <santiago.anda@outlook.com>
2020-05-26 14:07:07 +0100

    ADD PREDICT WINNER

---
+++ DeepUAge1.0.py
@@ -0,0 +1,102 @@
+import argparse
+import os
+from pathlib import Path
+import numpy as np
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint, EarlyStopping
+from keras.optimizers import SGD
+import config
+from generator import FaceGenerator, ValGenerator
+from model import get_model, age_mae
+
+image_directory = os.path.expanduser(config.image_directory)
+
+list_images = [x for x in os.listdir(os.path.join(image_directory, 'train')) if not x.startswith('.')]
+nb_classes = len(list_images)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="This script trains the CNN model for age estimation.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--pred_dir", type=str,
+                        help="path to the Predator face dataset",
+                        default=image_directory)
+
+    parser.add_argument("--output_dir", type=str, default="checkpoints",
+                        help="checkpoint dir")
+    parser.add_argument("--batch_size", type=int, default=config.batch_size,
+                        help="batch size")
+    parser.add_argument("--nb_epochs", type=int, default=config.MAXIMUM_EPOCHS,
+                        help="number of epochs")
+    parser.add_argument("--lr", type=float, default=0.1,
+                        help="learning rate")
+    parser.add_argument("--model_name", type=str, default="ResNet50",
+                        help="model name: 'ResNet50' or 'InceptionResNetV2'")
+    args = parser.parse_args()
+    return args
+
+
+class Schedule:
+    def __init__(self, nb_epochs, initial_lr):
+        self.epochs = nb_epochs
+        self.initial_lr = initial_lr
+
+    def __call__(self, epoch_idx):
+        if epoch_idx < self.epochs * 0.25:
+            return self.initial_lr
+        elif epoch_idx < self.epochs * 0.50:
+            return self.initial_lr * 0.2
+        elif epoch_idx < self.epochs * 0.75:
+            return self.initial_lr * 0.04
+        return self.initial_lr * 0.008
+
+
+def main():
+    args = get_args()
+
+    predator_dir = args.pred_dir
+
+    model_name = args.model_name
+    batch_size = args.batch_size
+    nb_epochs = args.nb_epochs
+    lr = args.lr
+
+    image_size = config.IMAGE_SIZE
+
+    train_gen = FaceGenerator(predator_dir, batch_size=batch_size, image_size=image_size, number_classes=nb_classes)
+    val_gen = ValGenerator(predator_dir, batch_size=batch_size, image_size=image_size, number_classes=nb_classes)
+
+    model = get_model(model_name=model_name, image_size=image_size, number_classes=nb_classes)
+
+    sgd = SGD(lr=0.1, momentum=0.9, nesterov=True)
+
+    model.compile(optimizer=sgd, loss="categorical_crossentropy", metrics=[age_mae])
+
+    model.summary()
+
+    output_dir = Path(__file__).resolve().parent.joinpath(args.output_dir)
+
+    if not output_dir.exists():
+        output_dir.mkdir(parents=True)
+
+    callbacks = [EarlyStopping(monitor='val_age_mae', mode='min', verbose=1, patience=5),
+                 LearningRateScheduler(schedule=Schedule(nb_epochs, initial_lr=lr)),
+                 ModelCheckpoint(str(output_dir) + "]/weights.{epoch:03d}-{val_loss:.3f}-{"
+                                                   "val_age_mae:.3f}.hdf5",
+                                 monitor="val_age_mae",
+                                 verbose=1,
+                                 save_best_only=True,
+                                 mode="min")
+                 ]
+
+    hist = model.fit_generator(generator=train_gen,
+                               epochs=nb_epochs,
+                               validation_data=val_gen,
+                               verbose=1,
+                               callbacks=callbacks)
+
+    np.savez(str(output_dir.joinpath("history.npz")), history=hist.history)
+
+
+if __name__ == '__main__':
+    main()
---
+++ generator.py
@@ -0,0 +1,144 @@
+import os
+
+import Augmentor
+import random
+from PIL import Image
+import numpy as np
+import cv2
+from keras.utils import Sequence, to_categorical
+
+
+def get_transform_func():
+    p = Augmentor.Pipeline()
+    p.flip_left_right(probability=0.5)
+    p.rotate(probability=1, max_left_rotation=5, max_right_rotation=5)
+    p.zoom_random(probability=0.5, percentage_area=0.95)
+    p.random_distortion(probability=0.5, grid_width=2, grid_height=2, magnitude=8)
+    p.random_color(probability=1, min_factor=0.8, max_factor=1.2)
+    p.random_contrast(probability=1, min_factor=0.8, max_factor=1.2)
+    p.random_brightness(probability=1, min_factor=0.8, max_factor=1.2)
+    p.random_erasing(probability=0.5, rectangle_area=0.2)
+
+    def transform_image(image):
+        image = [Image.fromarray(image)]
+        for operation in p.operations:
+            r = round(random.uniform(0, 1), 1)
+            if r <= operation.probability:
+                image = operation.perform_operation(image)
+        return image[0]
+    return transform_image
+
+
+class FaceGenerator(Sequence):
+    def __init__(self, predator_dir, batch_size=32, image_size=224, number_classes=101, regression=False):
+        self.image_path_and_age = []
+        self._load_predator(predator_dir)
+        self.image_num = len(self.image_path_and_age)
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.indices = np.random.permutation(self.image_num)
+        self.transform_image = get_transform_func()
+        self.number_classes = number_classes
+        self.regression = regression
+
+    def __len__(self):
+        return self.image_num // self.batch_size
+
+    def __getitem__(self, idx):
+        batch_size = self.batch_size
+        image_size = self.image_size
+        x = np.zeros((batch_size, image_size, image_size, 3), dtype=np.uint8)
+        y = np.zeros((batch_size, 1), dtype=np.int32)
+
+        sample_indices = self.indices[idx * batch_size:(idx + 1) * batch_size]
+
+        for i, sample_id in enumerate(sample_indices):
+            image_path, age = self.image_path_and_age[sample_id]
+
+            greyscale_image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+
+            image = np.repeat(greyscale_image[..., np.newaxis], 3, -1)
+
+            x[i] = self.transform_image(cv2.resize(image, (image_size, image_size)))
+            y[i] = age
+
+        #return x, to_categorical(y, 101)
+
+        if self.regression:
+            return x, y
+        else:
+            return x, to_categorical(y, self.number_classes)
+
+    def on_epoch_end(self):
+        self.indices = np.random.permutation(self.image_num)
+
+    def _load_predator(self, predator_dir):
+        predator_root = predator_dir
+
+        train_image_dir = os.path.join(predator_root, 'train')
+
+        for root, dirs, _ in os.walk(train_image_dir):
+            for d in dirs:
+                for x in os.listdir(os.path.join(train_image_dir, d)):
+                    if not x.startswith('.'):
+                        age = d
+                        image_name = x
+
+                        file_path = os.path.join(root, age, image_name)
+
+                        if os.path.isfile(file_path):
+                            self.image_path_and_age.append([str(file_path), age])
+
+
+class ValGenerator(Sequence):
+    def __init__(self, predator_dir, batch_size=32, image_size=224, number_classes=101, regression=False):
+        self.image_path_and_age = []
+        self._load_predator(predator_dir)
+        self.image_num = len(self.image_path_and_age)
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.number_classes = number_classes
+        self.regression = regression
+
+    def __len__(self):
+        return self.image_num // self.batch_size
+
+    def __getitem__(self, idx):
+        batch_size = self.batch_size
+        image_size = self.image_size
+        x = np.zeros((batch_size, image_size, image_size, 3), dtype=np.uint8)
+        y = np.zeros((batch_size, 1), dtype=np.int32)
+
+        for i in range(batch_size):
+            image_path, age = self.image_path_and_age[idx * batch_size + i]
+
+            greyscale_image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+
+            image = np.repeat(greyscale_image[..., np.newaxis], 3, -1)
+
+            x[i] = cv2.resize(image, (image_size, image_size))
+            y[i] = age
+
+        #return x, to_categorical(y, 101)
+
+        if self.regression:
+            return x, y
+        else:
+            return x, to_categorical(y, self.number_classes)
+
+    def _load_predator(self, predator_dir):
+        predator_root = predator_dir
+
+        test_image_dir = os.path.join(predator_root, 'test')
+
+        for root, dirs, _ in os.walk(test_image_dir):
+            for d in dirs:
+                for x in os.listdir(os.path.join(test_image_dir, d)):
+                    if not x.startswith('.'):
+                        age = d
+                        image_name = x
+
+                        file_path = os.path.join(root, age, image_name)
+
+                        if os.path.isfile(file_path):
+                            self.image_path_and_age.append([str(file_path), age])
