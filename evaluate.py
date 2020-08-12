23523ee9b1703e54255f86b859bd5eeb6c6463a0
santiago <santiago.anda@outlook.com>
2020-08-12 11:26:48 +0100

    Add log functionality

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,8 @@
 
 # create network
 import os
+
+import neptune
 import sklearn.metrics
 import numpy as np
 from keras import optimizers
@@ -13,21 +15,43 @@ import seaborn as sn
 import pandas as pd
 import csv
 from model import age_mae
-
 import config
 
+# log the evaluation
 
-weights_filename = 'weights.030-2.360-1.540'
 
-model = load_model('models/{}.hdf5'.format(weights_filename), custom_objects={'age_mae': age_mae})
+log_results = config.LOG_RESULTS
+
+weights_filename = 'weights.087-1.799-1.528'
+
+experiment_name = 'SAN-278'
+
+model = load_model('checkpoints/{}/{}.hdf5'.format(experiment_name, weights_filename), custom_objects={'age_mae': age_mae})
 
 image_path = os.path.expanduser(config.IMAGE_PATH)
 test_path = os.path.join(image_path, 'test')
-datagen_batch_size = 64
 batch_size = 64
 image_size = 224
 learning_rate = 0.1
 momentum = 0.9
+
+
+PARAMS = {
+        'batch_size': batch_size,
+        'learning_rate': learning_rate,
+        'momentum': momentum,
+        'image_size': image_size,
+        'test_path': image_path
+    }
+
+if log_results:
+
+    neptune.init(project_qualified_name='4ND4/sandbox')
+    #neptune_tb.integrate_with_keras()
+    result = neptune.create_experiment(name='evaluation_{}'.format(experiment_name), params=PARAMS, tags='evaluation')
+    name = result.id
+else:
+    name = 'debug'
 
 datagen = ImageDataGenerator()
 
@@ -47,7 +71,11 @@ model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=[age
 true_labels = test_generator.classes
 
 scores = model.evaluate_generator(test_generator, test_generator.samples)
-print("MAE = ", scores[1])
+MAE = scores[1]
+
+print("MAE = ", MAE)
+
+neptune.log_metric('mean_absolute_error', MAE)
 
 predictions = model.predict_generator(test_generator)
 
@@ -56,7 +84,7 @@ y_pred = np.array([np.argmax(x) for x in predictions])
 
 # get values and copy to
 
-with open('evaluation_{}.csv'.format(weights_filename), 'w') as f1:
+with open('evaluation_{}_{}.csv'.format(name, weights_filename), 'w') as f1:
     writer = csv.writer(f1, delimiter='\t', lineterminator='\n', )
 
     for i in range(0, len(y_pred)):
